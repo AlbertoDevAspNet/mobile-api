46802772b3e50a8a63120ef7ff692dea34eed048
Alberto Lacerda <albertotads@outlook.com>
2026-04-22 20:50:07 -0300

    Configurado Arquivo Classe Singletom

--- app/src/main/java.com.example.userlistapp/api/RetrofitClient.kt
+++ app/src/main/java.com.example.userlistapp/api/RetrofitClient.kt
@@ -1,1 +1,27 @@
+package java.com.example.userlistapp.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+/**
+** Singleton para criar e fornecer uma instância do Retrofit
+* Configura o Retrofit com a URL base e o conversor Gson
+* @author Alberto
+**/
+object RetrofitClient {
+    private const val BASE_URL = "https://api.example.com/"
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            
+    }
+    // Cria uma instância do ApiService usando o Retrofit
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}     
